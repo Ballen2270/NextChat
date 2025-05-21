149a2a390b7ed6f82ff9e4e10f56dfd0092e14b3
Ballen2270 <chinapat200065@icloud.com>
2025-05-21 16:09:11 +0800

    add gemini 2.5 flash

--- app/constant.ts
+++ app/constant.ts
@@ -546,8 +546,7 @@ const googleModels = [
   "gemini-2.0-flash-thinking-exp-01-21",
   "gemini-2.0-pro-exp",
   "gemini-2.0-pro-exp-02-05",
-  "gemini-2.5-flash-preview-04-17",
-  "gemini-2.5-pro-experimental-03-25"
+  "gemini-2.5-flash-preview-05-20",
 ];
 
 const anthropicModels = [
